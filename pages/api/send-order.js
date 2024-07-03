9012974fe554d5506345c27b9e24364c45d219d6
Peter <pedromoyano454@gmail.com>
2024-07-03 05:39:37 -0300

    Change receiver

--- pages/api/send-order.js
+++ pages/api/send-order.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
       await client.messages.create({
         body: message,
         from: process.env.TWILIO_WHATSAPP_NUMBER,
-        to: process.env.RECEIVER_WHATSAPP_NUMBER,
+							to: process.env.NEXT_PUBLIC_RECEIVER_WHATSAPP_NUMBER,
       });
       res.status(200).json({ success: true });
 
